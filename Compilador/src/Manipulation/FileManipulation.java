e0d57a591aea3fd0bd9f89237a217e9a88b1011f
ChaconArthur <arthurcesar8@gmail.com>
2015-04-27 16:38:28 -0300

    Classe FileManipulation

---
+++ Compilador/src/Manipulation/FileManipulation.java
@@ -0,0 +1,32 @@
+package Manipulation;
+
+import java.io.*;
+
+
+/**
+ *
+ * @author chacon
+ * 
+ */
+public class FileManipulation {
+    
+    public static void Reader (String path /*Por o caminho do arquivo aqui*/) throws IOException {
+        
+        try (BufferedReader BuffReader = new BufferedReader(new FileReader(path /*Por o caminho do arquivo aqui*/))) {
+            String line = "";
+            
+            while (true){
+                if (line != null){
+                    System.out.println(line);
+                    
+                }else{
+                    break;
+                }
+                line = BuffReader.readLine();
+            }
+        }catch (IOException e) { 
+            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+        }            
+    }
+    
+}
